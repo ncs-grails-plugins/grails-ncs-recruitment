48f5bee138bb5b9f7331fb6e2767c0ae5e987a46
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-03-27 10:32:14 -0500

    changed comments to JavaDoc format

--- grails-app/domain/edu/umn/ncs/DwellingSegment.groovy
+++ grails-app/domain/edu/umn/ncs/DwellingSegment.groovy
@@ -1,24 +1,23 @@
 package edu.umn.ncs
 
+/** This is used to determine the selection
+ * for the initial mailings */
 class DwellingSegment {
 	
-    // This is used to determine the selection
-    // for the initial mailings
-	
-    // Tie this selection to the address
+    /** Tie this selection to the address */
     DwellingUnit dwellingUnit
 
-    // Tie the segment to the address
+    /** Tie the segment to the address */
     RecruitmentSegment segment
 
-    // Assign a recruitment methodology
+    /** Assign a recruitment methodology */
     RecruitmentGroup recruitmentGroup
 
-    // This is probably shouldn't be here.
-    // Does this address get a pregenancy screener incentive
+    /** This is probably shouldn't be here.
+     * Does this address get a pregenancy screener incentive */
     Boolean pregnancyScreenerIncentive
 
-    // This is the random selection order chosen by Simone Q. Vuong
+    /** This is the random selection order chosen by Simone Q. Vuong */
     Integer randomizedId  = new Random(System.currentTimeMillis()).nextInt(10000000)
 
     static constraints = {
